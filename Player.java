c383fb9fdd52c2cb6289bd16b2e9517731d7e986
iyers16 <62408826+iyers16@users.noreply.github.com>
2021-09-13 14:27:33 -0400

    Update and rename src/game/Player.java to Player.java

---
+++ Player.java
@@ -0,0 +1,18 @@
+
+package gamegrid;
+
+public class Player extends Sprite {
+   
+    public float magicPower;
+    
+    public Player() {
+        positionx = 4;
+        positiony = 1;
+        strenght = 10;
+        name = "Chronos";
+        health = 50;
+        magicPower = 3.5f;
+        
+        
+    }
+}
