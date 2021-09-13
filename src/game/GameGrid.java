678d180012e89a57d789f654e9a267e601b2a007
iyers16 <62408826+iyers16@users.noreply.github.com>
2021-09-13 14:24:33 -0400

    Update and rename Game.java to GameGrid.java

--- src/game/GameGrid.java
+++ src/game/GameGrid.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package game;
+package gamegrid;
 
 /**
  *
  * @author cstuser
  */
-public class Game {
+public class GameGrid {
 
     /**
      * @param args the command line arguments
